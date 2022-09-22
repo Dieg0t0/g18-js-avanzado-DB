29fff9ed80569cc39e132c79783899d08538f4ed
Diego Bustamante <diegob88891@gmail.com>
2022-09-21 20:19:48 -0500

    feat: poniendome al dia

--- 1-Node/1.2-crear-archivo/index.js
+++ 1-Node/1.2-crear-archivo/index.js
@@ -1,14 +1,10 @@
-const fs = require('fs');
+const fs= require('fs');
+const path= './';
+const filename= 'my-file.txt';
+const contentFile= 'Queremos ir al receso';
+const destination= `${path}${filename}`;
 
-const path ='./';
-const fileName = 'my-file.txt'
-const contentFile = 'ya queremos ir a receso'
-const destination = `${path}${fileName}`
-
-fs.writeFile(destination, contentFile, (err)=> {
-    if(err){
-        throw err;
-    } else {
-        console.log('archivo creado con exito! :)')
-    }
+fs.writeFile(destination, contentFile, (err) => {
+    if (err) throw err;
+    console.log('Archivo creado con exito');
 })
